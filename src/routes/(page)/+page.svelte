ddf08d7526259a47aab60b5bb4dac8c69078c9fa
Peter Pakanun <lpeachl@gmail.com>
2025-05-02 23:08:44 +0700

    refactor layout to use Card component structure

--- src/routes/(page)/+page.svelte
+++ src/routes/(page)/+page.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
 </script>
 
-<Button>Click me</Button>
+<div class="container h-full py-6">
+	<div class="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
+		<Card.Root class="order-1">
+			<Card.Header>
+				<Card.Title>Card Title</Card.Title>
+				<Card.Description>Card Description</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<p>Card Content</p>
+			</Card.Content>
+			<Card.Footer>
+				<p>Card Footer</p>
+			</Card.Footer>
+		</Card.Root>
+		<div class="order-2 flex flex-col space-y-4">test</div>
+	</div>
+</div>
