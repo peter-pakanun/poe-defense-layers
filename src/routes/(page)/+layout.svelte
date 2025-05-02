8606843acb50f567d263f5ff3fd33a9e9f4fa592
Peter Pakanun <lpeachl@gmail.com>
2025-05-02 22:45:37 +0700

    add separator component and integrate it into layout

---
+++ src/routes/(page)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+
+	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+</script>
+
+<div class="container space-y-4">
+	<h4 class="text-4xl">PoE Defense Layers Visualizer</h4>
+	<Separator />
+	{@render children()}
+</div>
