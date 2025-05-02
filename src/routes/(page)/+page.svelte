f7f2518289641639d39fb00a9f4c8da62033afe4
Peter Pakanun <lpeachl@gmail.com>
2025-05-02 23:38:14 +0700

    update armour state initialization and enhance MySlider component with min/max props

--- src/routes/(page)/+page.svelte
+++ src/routes/(page)/+page.svelte
@@ -2,25 +2,22 @@
 	import * as Card from '$lib/components/ui/card/index.js';
 	import MySlider from './MySlider.svelte';
 
-	let armour = $state(2000);
+	let armour = $state(3000);
+	let regen = $state(200);
 </script>
 
 <div class="container h-full py-6">
 	<div class="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
-		<Card.Root class="order-1">
-			<Card.Header>
-				<Card.Title>Card Title</Card.Title>
-				<Card.Description>Card Description</Card.Description>
-			</Card.Header>
+		<Card.Root class="order-1 aspect-video">
 			<Card.Content>
-				<p>{armour}</p>
+				<div class="relative h-full w-full">
+					<div class="absolute left-[10%] top-0 bg-red-500">A</div>
+				</div>
 			</Card.Content>
-			<Card.Footer>
-				<p>Card Footer</p>
-			</Card.Footer>
 		</Card.Root>
 		<div class="order-2 flex flex-col space-y-4">
 			<MySlider label="Armour" id="armour" bind:value={armour} />
+			<MySlider label="Regeneration" id="regen" bind:value={regen} min={0} max={3000} />
 		</div>
 	</div>
 </div>
--- src/routes/(page)/MySlider.svelte
+++ src/routes/(page)/MySlider.svelte
@@ -7,15 +7,19 @@
 		value = $bindable(2000),
 		label = 'Label',
 		id = 'id',
+		min = 0,
+		max = 30000,
 	}: {
 		value?: number;
 		label?: string;
 		id?: string;
+		min?: number;
+		max?: number;
 	} = $props();
 </script>
 
 <div class="grid w-full max-w-sm items-center gap-2">
 	<Label for={id}>{label}</Label>
-	<Input {id} type="number" bind:value min={0} max={30000} step={100} />
-	<Slider type="single" bind:value min={0} max={30000} step={100} />
+	<Input {id} type="number" bind:value {min} {max} step={100} />
+	<Slider type="single" bind:value {min} {max} step={100} />
 </div>
